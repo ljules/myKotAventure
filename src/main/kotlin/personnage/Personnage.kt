c6ee0d360287d4caf7816238d0ecc0af96ea72bf
Moulin Timothée <timomoulin@msn.com>
2023-09-25 00:07:27 +0200

    initial

---
+++ src/main/kotlin/personnage/Personnage.kt
@@ -0,0 +1,31 @@
+package personnage
+
+ class Personnage(
+    val nom: String,
+    var pointDeVie: Int,
+    val pointDeVieMax: Int,
+    var attaque: Int,
+    var defense: Int,
+    var endurance: Int,
+    var vitesse: Int,
+) {
+
+     fun calculeDefense():Int{
+         //TODO
+        return this.defense/2;
+     }
+
+     // Méthode pour attaquer un adversaire
+     fun attaque(adversaire: Personnage) {
+        //TODO Mission 3.1
+       val degats= this.attaque/2
+         adversaire.pointDeVie-=degats
+        println("$nom attaque ${adversaire.nom} avec une attaque de base et inflige $degats points de dégâts.")
+    }
+
+    override fun toString(): String {
+        return "$nom (PV: $pointDeVie/$pointDeVieMax, Attaque: $attaque, Défense: $defense, Endurance: $endurance, Vitesse: $vitesse)"
+    }
+
+
+}
