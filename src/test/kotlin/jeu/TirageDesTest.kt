23803fcd45c7bfebc4fbcd2ec02b984ca08a702f
Moulin Timothée <timomoulin@msn.com>
2023-09-25 23:18:00 +0200

    commentaire test unitaire TirageDesTest.kt

--- src/test/kotlin/jeu/TirageDesTest.kt
+++ src/test/kotlin/jeu/TirageDesTest.kt
@@ -4,18 +4,26 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 internal class TirageDesTest {
-
+    // Test unitaire pour le tirage de Des
     @Test
     fun lance() {
-        // Test unitaire pour le tirage de Des
-        // Exemple : 3d9
+        // Création d'un objet TirageDes pour simuler un lancé de dés 3d9
         val des = TirageDes(3, 9)
-        // valeur minimum = 3
-        // valeur maximum = 3*9 =27
 
+        // Utilisation de la méthode lance() pour obtenir le résultat du lancé
         val resultat = des.lance()
         println(" result = $resultat")
+        // Le résultat doit être compris entre 3 (minimum possible avec 3 dés) et 27 (maximum possible avec 3 dés à 9 faces)
+        // valeur minimum = 3*1
+        // valeur maximum = 3*9 =27
         Assertions.assertTrue(resultat >= 3)
         Assertions.assertTrue(resultat <= 27)
+        // ou
+        //Assertions.assertTrue(resultat in (3..27))
+
+        Assertions.assertEquals("","")
+        Assertions.assertNotEquals("a",'a')
+        Assertions.assertNotNull("machin")
+
     }
 }
